4bc3fb66640d37e24b263e3a7938ce58bf2d9302
Raeid <raeidus15@gmail.com>
2024-03-10 01:50:18 -0500

    update

---
+++ build/CMakeFiles/newProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newProject.dir/main.cpp.o"
+  "CMakeFiles/newProject.dir/main.cpp.o.d"
+  "newProject"
+  "newProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/raeidusmanali/Documents/Coding/ICS4U0 Projects/grades-assignment-v0.1
+# Build directory: /Users/raeidusmanali/Documents/Coding/ICS4U0 Projects/grades-assignment-v0.1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
